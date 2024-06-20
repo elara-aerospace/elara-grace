3c0959c9ad8872f1b51926c30f74ea18d33400c4
Suriyaa Sundararuban <github@suriyaa.tk>
2024-06-20 13:57:00 +0200

    Consider using 'with' for resource-allocating operations and UTF-8 encoding for 'open' in setup.py
    - R1732: Consider using 'with' for resource-allocating operations (consider-using-with)
    - W1514: Using open without explicitly specifying an encoding (unspecified-encoding)

--- setup.py
+++ setup.py
@@ -9,15 +9,19 @@ Copyright (c) 2024 Elara Aerospace team
 
 from setuptools import setup, find_packages
 
+# Read the long description from the README.md file
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+# Read the requirements from the requirements.txt file
+with open('requirements.txt', 'r', encoding='utf-8') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='elara-grace',
     version='1.0.0',
     packages=find_packages(),
-    install_requires=[
-        'pylint',
-        'numpy',
-        'pandas'
-    ],
+    install_requires=requirements,
     entry_points={
         'console_scripts': [
             'elara-grace = src.main:main'
@@ -31,7 +35,7 @@ setup(
     author='Suriyaa Sundararuban',
     author_email='suriyaa.sundararuban@elara-aerospace.com',
     description='Rocket Fault Detection and Handling Software by Elara Aerospace',
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/elara-aerospace/elara-grace',
     classifiers=[
