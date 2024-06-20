4e91170a7bd07911e77443c92aa474e02be88b81
Suriyaa Sundararuban <github@suriyaa.tk>
2024-06-20 13:48:09 +0200

    Add docstring to settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,2 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""
+settings.py - Configuration settings
+
+Copyright (c) 2024 Suriyaa Sundararuban
+Copyright (c) 2024 Elara Aerospace team
+"""
+
 # Configuration settings for the rocket monitoring system
 LOG_FILE = 'logs/rocket_monitor.log'
