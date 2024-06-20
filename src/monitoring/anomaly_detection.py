282ae4edce91eeb975ab22076c130e2879335c03
Suriyaa Sundararuban <github@suriyaa.tk>
2024-06-20 13:52:46 +0200

    Remove trailing whitespaces in anomaly_detection.py

--- src/monitoring/anomaly_detection.py
+++ src/monitoring/anomaly_detection.py
@@ -10,14 +10,14 @@ Copyright (c) 2024 Elara Aerospace team
 class AnomalyDetector:
     def detect(self, sensor_data):
         anomalies = {}
-        
+
         if sensor_data['temperature'] < -40 or sensor_data['temperature'] > 120:
             anomalies['temperature'] = sensor_data['temperature']
-        
+
         if sensor_data['pressure'] < 5 or sensor_data['pressure'] > 150:
             anomalies['pressure'] = sensor_data['pressure']
-        
+
         if sensor_data['altitude'] < 0 or sensor_data['altitude'] > 9000:
             anomalies['altitude'] = sensor_data['altitude']
-        
+
         return anomalies
