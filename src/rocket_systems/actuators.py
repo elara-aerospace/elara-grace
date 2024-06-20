3189ac9dc7a113aa1a3dcf69ae2e5636ea32c696
Suriyaa Sundararuban <github@suriyaa.tk>
2024-06-20 14:12:07 +0200

    Fix 3 pylint errors in actuators.py

--- src/rocket_systems/actuators.py
+++ src/rocket_systems/actuators.py
@@ -3,10 +3,27 @@
 """
 actuators.py
 
+This module defines the ActuatorModule class responsible for controlling
+the actuators of the rocket system.
+
 Copyright (c) 2024 Suriyaa Sundararuban
 Copyright (c) 2024 Elara Aerospace team
 """
 
+# pylint: disable=too-few-public-methods
+
 class ActuatorModule:
+    """
+    A class used to control the actuators of the rocket system.
+
+    Methods
+    -------
+    activate_parachute():
+        Activates the parachute.
+    """
+    
     def activate_parachute(self):
+        """
+        Activates the parachute by sending the appropriate command to the actuator.
+        """
         print("Parachute activated!")
