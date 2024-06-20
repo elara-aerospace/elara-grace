598b8f269ffa7f21e2f1fc52a079b10b184112e4
Suriyaa Sundararuban <github@suriyaa.tk>
2024-06-20 14:06:44 +0200

    Fix pylint errors (C0303, E0401, C0115, C0116 and R0903) in contingency_actions.py

--- src/monitoring/contingency_actions.py
+++ src/monitoring/contingency_actions.py
@@ -7,21 +7,34 @@ Copyright (c) 2024 Suriyaa Sundararuban
 Copyright (c) 2024 Elara Aerospace team
 """
 
+# pylint: disable=too-few-public-methods
+
 from rocket_systems.actuators import ActuatorModule
 from rocket_systems.communications import CommunicationsModule
 
 class ContingencyActions:
+    """
+    A class used to execute contingency actions based on detected anomalies.
+    """
+    
     def __init__(self):
+        """
+        Initializes the ContingencyActions with instances of ActuatorModule
+        and CommunicationsModule.
+        """
         self.actuator = ActuatorModule()
         self.communications = CommunicationsModule()
 
     def execute(self, anomalies):
+        """
+        Executes the appropriate actions based on the detected anomalies.
+        """
         if 'temperature' in anomalies:
             self.communications.send_data(f"High temperature detected: {anomalies['temperature']}")
-        
+
         if 'pressure' in anomalies:
             self.communications.send_data(f"Abnormal pressure detected: {anomalies['pressure']}")
-        
+
         if 'altitude' in anomalies:
             self.communications.send_data(f"Abnormal altitude detected: {anomalies['altitude']}")
             self.actuator.activate_parachute()
