530c4623eeab1514a06cf6ccd11795f2af3c64d4
Suriyaa Sundararuban <github@suriyaa.tk>
2024-06-20 14:19:02 +0200

    Fix 3 errors in sensors.py

--- src/rocket_systems/sensors.py
+++ src/rocket_systems/sensors.py
@@ -3,14 +3,26 @@
 """
 sensors.py
 
+This module defines the SensorModule class responsible
+for reading sensor data from the rocket system.
+
 Copyright (c) 2024 Suriyaa Sundararuban
 Copyright (c) 2024 Elara Aerospace team
 """
 
+# pylint: disable=too-few-public-methods
+
 import random
 
 class SensorModule:
+    """
+    A class used to read sensor data from the rocket system.
+    """
+    
     def read_sensors(self):
+        """
+        Reads the real-time values from the sensors.
+        """
         data = {
             'temperature': random.uniform(-50, 150),
             'pressure': random.uniform(0, 200),
