21dda6278b71888d431e85a286e91a2bed7d8e18
Suriyaa Sundararuban <github@suriyaa.tk>
2024-06-20 14:14:22 +0200

    Fix 3 errors in communications.py

--- src/rocket_systems/communications.py
+++ src/rocket_systems/communications.py
@@ -3,10 +3,32 @@
 """
 communications.py
 
+This module defines the CommunicationsModule class responsible for handling
+the communication operations of the rocket system.
+
 Copyright (c) 2024 Suriyaa Sundararuban
 Copyright (c) 2024 Elara Aerospace team
 """
 
+# pylint: disable=too-few-public-methods
+
 class CommunicationsModule:
+    """
+    A class used to handle communication operations of the rocket system.
+
+    Methods
+    -------
+    send_data(data):
+        Sends data to the communication system.
+    """
+    
     def send_data(self, data):
+        """
+        Sends data to the communication system.
+
+        Parameters
+        ----------
+        data : str
+            The data to be sent.
+        """
         print(f"Sending data: {data}")
